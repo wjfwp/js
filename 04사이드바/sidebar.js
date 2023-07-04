ac6e37319246493351f8ba0038052565e3083455
wjfwp <sis6710@gmail.com>
2023-07-04 16:15:35 +0900

    7/4

---
+++ 04사이드바/sidebar.js
@@ -0,0 +1,34 @@
+var menuBtn = document.getElementById("menuBtn");
+var sidenav = document.querySelector(".sidenav");
+
+menuBtn.addEventListener('click', function() {
+    console.log(sidenav.style.width);
+
+    // var wid = sidenav.style.width;
+    // if(wid === '' || wid === '200px') {
+    //     sidenav.style.width = '0px';
+    // } else {
+    //     sidenav.style.width = '200px';
+    // }
+
+    console.log(window.innerWidth); //브라우저의 너비
+    var wid = sidenav.style.width;
+
+    if (window.innerWidth < 767) { //모바일
+
+        if(wid === '' || wid ==='0px'){
+            sidenav.style.width = '200px';
+        } else {
+            sidenav.style.width = '0px';
+
+        }
+
+    } else { //브라우저 
+
+        if (wid === '' || wid === '200px') {
+            sidenav.style.width = '0px';
+        } else {
+            sidenav.style.width = '200px';
+        }
+    }
+});
